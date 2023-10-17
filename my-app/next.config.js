4f921d4a1f72fb337bc7ff087861f224e157ccca
Mansur Dewan <mansur.dewan@indusnet.co.in>
2023-10-17 13:29:40 +0530

    next js ready

---
+++ my-app/next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 1,
+  },
+  staticPageGenerationTimeout: 120,
+
+  reactStrictMode: false,
+
+  env: {
+    BASE_URL: "http://127.0.0.1:2233",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wocamc-bucket-cms-uat.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+    ],
+  },
+
+  output: "standalone",
+};
+
+module.exports = nextConfig;
---
+++ my-app/src/app/page.js
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import axios from "axios";
+const baseUrl = process.env.BASE_URL;
+const AxiosInstance = axios.create({
+  baseURL: "http://localhost:2233",
+});
+
+export const getAllData = async () => {
+  const { data } = await AxiosInstance.get("/get-all-person");
+  return data;
+};
+
+export default async function Home() {
+  const data = await getAllData();
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        {data?.data?.map((item) => {
+          return (
+            <>
+              <hr />
+              <h1>{item?.FirstName}</h1>
+              <h1>{item?.LastName}</h1>
+              <p>City: {item?.City}</p>
+              <p>State: {item?.Address}</p>
+              <hr />
+            </>
+          );
+        })}
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer">
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer">
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer">
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer">
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
